bb9ae0e4f16320e625993e4b05dc27d9b245159f
Carlson <carlsonyuandev@gmail.com>
2025-07-15 23:22:28 +0800

    refactor: use ModerationAction enum instead of string literals

--- imports/types/ModerationPolicy.ts
+++ imports/types/ModerationPolicy.ts
@@ -1,6 +1,12 @@
+export enum ModerationAction {
+  NoAction = 'No Action',
+  Replace = 'Replace With Asterisks (*)',
+  Block = 'Block From Sending'
+}
+
 export type ModerationPolicy = {
     createdAt: Date;
     type: string; // e.g., 'txt', 'image', etc.
-    action: string;
+    action: ModerationAction;
     fields: string; // e.g., ['msg', 'caption']
 }
